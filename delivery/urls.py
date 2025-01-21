00f2cf99b27ade1448ed6cf856e4c16322973e07
deep473 <deepztanshu@gmail.com>
2025-01-21 16:32:44 +0530

    update & delete

--- delivery/urls.py
+++ delivery/urls.py
@@ -2,14 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('signin/', views.signin),
-    path('signup/', views.signup),
-    path('handle_login/', views.handle_login, name='handle_login'),
-    path('handle_signup/', views.handle_signup, name='handle_signup'),
-    path('handle_login/restaurant_page/', views.restaurant_page, name='restaurant_page/'),
-    path('handle_login/add_restaurant/', views.add_restaurant, name='add_restaurant'),
-    path('handle_login/show_restaurant_page/', views.show_restaurant_page, name='show_restaurant_page/'),
-    
-    path('restaurant/<int:restaurant_id>/menu/', views.restaurant_menu, name='restaurant_menu'),
+    # Home and authentication paths
+    path('', views.index, name='index'),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.handle_login, name='handle_login'),
+    path('signup/submit/', views.handle_signup, name='handle_signup'),
+
+    # Restaurant-related paths
+    path('restaurants/', views.show_restaurant_page, name='show_restaurant_page'),
+    path('restaurants/add/', views.add_restaurant, name='add_restaurant'),
+    path('restaurants/<int:restaurant_id>/', views.restaurant_page, name='restaurant_page'),
+    path('restaurants/<int:restaurant_id>/menu/', views.restaurant_menu, name='restaurant_menu'),
+    path('restaurants/<int:restaurant_id>/update/', views.update_restaurant, name='update_restaurant'),
+    path('restaurants/<int:restaurant_id>/update/page/', views.update_restaurant_page, name='update_restaurant_page'),
+    path('restaurants/<int:restaurant_id>/delete/', views.delete_restaurant, name='delete_restaurant'),
+
+    path('menu/<int:menuItem_id>/update/', views.update_menuItem, name='update_menuItem'),
+    path('menu/<int:menuItem_id>/update/page/', views.update_menuItem_page, name='update_menuItem_page'),
+    path('menu/<int:menuItem_id>/delete/', views.delete_menuItem, name='delete_menuItem'),
 ]
--- delivery/views.py
+++ delivery/views.py
@@ -2,31 +2,33 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from delivery.models import Customer, Restaurant, MenuItem
 
-# Create your views here.
+# Home Page
 def index(request):
-    return render(request,'delivery/index.html')
+    return render(request, 'delivery/index.html')
 
+# Sign In Page
 def signin(request):
-    return render(request,'delivery/signin.html')
+    return render(request, 'delivery/signin.html')
 
+# Sign Up Page
 def signup(request):
-    return render(request,'delivery/signup.html')
+    return render(request, 'delivery/signup.html')
 
+# Handle Login
 def handle_login(request):
-
     if request.method == 'POST':
-        #Client's data
         username = request.POST.get('username')
         password = request.POST.get('password')
-      
+
         try:
-            cust = Customer.objects.get(username = username, password = password)
-            return render(request,'delivery/success.html')
-        except:
-            return render(request,'delivery/fail.html')
-    else:
-        return HttpResponse("Invalid request")
-    
+            # Check if the customer exists
+            Customer.objects.get(username=username, password=password)
+            return render(request, 'delivery/success.html')
+        except Customer.DoesNotExist:
+            return render(request, 'delivery/fail.html')
+    return HttpResponse("Invalid request")
+
+# Handle Sign Up
 def handle_signup(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -34,48 +36,49 @@ def handle_signup(request):
         email = request.POST.get('email')
         mobile = request.POST.get('mobile')
         address = request.POST.get('address')
-        #preventing duplicate username
-        try:
-            cust = Customer.objects.get(username = username)
-        except:
-            #username does not exist
-            c = Customer(username = username, password = password, email = email, mobile = mobile, address = address)
-            c.save()
 
-        return render(request,'delivery/signin.html')
-        
-    else:
-        return HttpResponse("Invalid request")
-    
+        # Check for duplicate username
+        if not Customer.objects.filter(username=username).exists():
+            Customer.objects.create(
+                username=username,
+                password=password,
+                email=email,
+                mobile=mobile,
+                address=address
+            )
+            return render(request, 'delivery/signin.html')
+
+    return HttpResponse("Invalid request")
+
+# Add Restaurant Page
 def restaurant_page(request):
-    return render(request,'delivery/add_restaurant.html')
-#from delivery.models import Customer, Restaurant
+    return render(request, 'delivery/add_restaurant.html')
+
+# Add Restaurant
 def add_restaurant(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         picture = request.POST.get('picture')
         cuisine = request.POST.get('cuisine')
         rating = request.POST.get('rating')
-        
-        rest = Restaurant(name = name, picture = picture, cuisine = cuisine, rating = rating)
-        rest.save()
+
+        Restaurant.objects.create(name=name, picture=picture, cuisine=cuisine, rating=rating)
 
         restaurants = Restaurant.objects.all()
+        return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
 
-        return render(request,'delivery/show_restaurants.html', {"restaurants":restaurants})
-        
-    else:
-        return HttpResponse("Invalid request")
+    return HttpResponse("Invalid request")
 
+# Show Restaurants
 def show_restaurant_page(request):
     restaurants = Restaurant.objects.all()
-    return render(request,'delivery/show_restaurants.html', {"restaurants":restaurants})
+    return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
 
+# Restaurant Menu
 def restaurant_menu(request, restaurant_id):
     restaurant = get_object_or_404(Restaurant, id=restaurant_id)
 
     if request.method == 'POST':
-        # Handle adding new menu item
         name = request.POST.get('name')
         description = request.POST.get('description')
         price = request.POST.get('price')
@@ -90,13 +93,67 @@ def restaurant_menu(request, restaurant_id):
             is_veg=is_veg,
             picture=picture
         )
-
         return redirect('restaurant_menu', restaurant_id=restaurant.id)
 
-    # Fetch all menu items for this restaurant
     menu_items = restaurant.menu_items.all()
-
     return render(request, 'delivery/menu.html', {
         'restaurant': restaurant,
         'menu_items': menu_items,
     })
+
+# Update Restaurant Page
+def update_restaurant_page(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    return render(request, 'delivery/update_restaurant_page.html', {"restaurant": restaurant})
+
+# Update Restaurant
+def update_restaurant(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+
+    if request.method == 'POST':
+        restaurant.name = request.POST.get('name')
+        restaurant.picture = request.POST.get('picture')
+        restaurant.cuisine = request.POST.get('cuisine')
+        restaurant.rating = request.POST.get('rating')
+        restaurant.save()
+
+        restaurants = Restaurant.objects.all()
+        return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
+
+# Delete Restaurant
+def delete_restaurant(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    restaurant.delete()
+
+    restaurants = Restaurant.objects.all()
+    return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
+
+
+# Update Menu item Page
+def update_menuItem_page(request, menuItem_id):
+    menuItem = get_object_or_404(MenuItem, id=menuItem_id)
+    return render(request, 'delivery/update_menuItem_page.html', {"item": menuItem})
+
+# Update MenuItem
+def update_menuItem(request, menuItem_id):
+    menuItem = get_object_or_404(MenuItem, id=menuItem_id)
+
+    if request.method == 'POST':
+        menuItem.name = request.POST.get('name')
+        menuItem.description = request.POST.get('description')
+        menuItem.price = request.POST.get('price')
+        menuItem.is_veg = request.POST.get('is_veg') == 'on'
+        menuItem.picture = request.POST.get('picture')
+
+        menuItem.save()
+
+        restaurants = Restaurant.objects.all()
+        return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
+
+# Delete menuItem
+def delete_menuItem(request, menuItem_id):
+    menuItem = get_object_or_404(MenuItem, id=menuItem_id)
+    menuItem.delete()
+
+    restaurants = Restaurant.objects.all()
+    return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
